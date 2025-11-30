9841f91d973c1738579864da50b2a332bba69088
VizualBeggar <aleksandr.golovnia@gmail.com>
2025-11-30 15:28:04 +0200

    Initial commit

--- Exercises/1-pipe.js
+++ Exercises/1-pipe.js
@@ -1,5 +1,13 @@
 'use strict';
 
-const pipe = (...fns) => (x) => null;
+const pipe = (...fns) => {
+  for (const fn of fns) {
+    if (typeof fn !== 'function') {
+      throw new TypeError('All arguments to pipe must be functions');
+    }
+  }
+
+  return (x) => fns.reduce((acc, fn) => fn(acc), x);
+};
 
 module.exports = { pipe };
--- Exercises/2-compose.js
+++ Exercises/2-compose.js
@@ -1,5 +1,28 @@
 'use strict';
+const EventEmitter = require('events');
 
-const compose = (...fns) => (x) => null;
+const compose = (...fns) => {
+  const emitter = new EventEmitter();
+
+  const wrapper = (x) => {
+    let result = x;
+    for (let i = fns.length - 1; i >= 0; i--) {
+      try {
+        result = fns[i](result);
+      } catch (e) {
+        emitter.emit('error', e);
+        return undefined;
+      }
+    }
+
+    return result;
+  };
+  wrapper.on = (event, listener) => {
+    emitter.on(event, listener);
+    return wrapper;
+  };
+
+  return wrapper;
+};
 
 module.exports = { compose };
